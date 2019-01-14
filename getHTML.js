2f63c6248595dc61456de81b4836cb3654779c6c
Dylan Leard <dleard@uvic.ca>
2019-01-14 19:56:50 +0000

    added step four - callbacks

---
+++ getHTML.js
@@ -0,0 +1,36 @@
+const https = require('https');
+
+let streamData = '';
+
+function getAndPrintHTML (options, callback) {
+
+  https.get(options, function(response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      streamData += data;
+    });
+
+    response.on('error', function(err) {
+      console.error(err);
+    });
+
+    response.on('end', function() {
+      callback(streamData);
+      console.log('Stream Finished');
+    });
+
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+const requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step2.html'
+};
+
+getAndPrintHTML(requestOptions, printHTML);
+
